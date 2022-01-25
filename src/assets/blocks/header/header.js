b7f8f82e10711baf409157384e3659df292bc21e
Lakson <imisn@mail.ru>
2022-01-26 01:44:50 +0300

    commit

--- src/assets/blocks/header/header.js
+++ src/assets/blocks/header/header.js
@@ -11,9 +11,12 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     let dropdown = document.querySelectorAll('.dropdown');
-
     dropdown.forEach(item => item.onclick = (item) => {
-        let open = item.target.classList.contains('dropdown_open')
+        if (!item.target.classList.contains('dropdown')) {
+            return
+        }
+        let open = item.target.classList.contains('dropdown_open');
+
         if (!open) {
             dropdown.forEach(item => {
                 item.classList.remove("dropdown_open");
@@ -26,4 +29,56 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     })
 
+
+    let languageCountry = document.querySelectorAll('.language__country');
+    languageCountry.forEach(item => {
+        let inputVal = item.querySelector('input')
+        item.onclick = (val) => {
+            languageCountry.forEach(item => {
+                item.classList.remove('language__country_active')
+            })
+
+            if (inputVal.checked) {
+
+                item.classList.add('language__country_active')
+
+                let languageBlock = document.querySelector('.language__block')
+                let languageFlag = languageBlock.querySelector('.language__flag')
+
+                languageFlag.classList.remove('language__flag_italia')
+                languageFlag.classList.remove('language__flag_france')
+                languageFlag.classList.remove('language__flag_deutschland')
+
+                switch (inputVal.value) {
+                    case '1':
+                        languageFlag.classList.add('language__flag_italia')
+                        break
+                    case '2':
+                        languageFlag.classList.add('language__flag_france')
+                        break
+                    case '3':
+                        languageFlag.classList.add('language__flag_deutschland')
+                        break
+                    default:
+                        languageFlag.classList.add('language__flag_italia')
+                }
+            }
+        }
+    })
+
+    let languageItem = document.querySelectorAll('.language__item');
+    languageItem.forEach(item => {
+
+        item.onclick = () => {
+            languageItem.forEach(item => {
+                item.classList.remove('language__item_active')
+            })
+            item.classList.add('language__item_active')
+
+            let languageBlock = document.querySelector('.language__block')
+            let languageText = languageBlock.querySelector('.language__text')
+            languageText.textContent = item.textContent
+
+        }
+    })
 })
